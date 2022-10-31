0540cedd714dedae323cee1c1756f69fbeaab2fa
Matias Zapata <matuzapata@gmail.com>
2022-10-31 20:53:25 -0300

    feat(docker**): Enable hot reaload development feat(App.js): Implement search api functionality feat(validateip): validate ip utility

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,52 @@
 import { useState } from "react";
 import { Box, Button, Container, Flex, Heading, IconButton, Input, Text } from "@chakra-ui/react";
 import { SearchIcon } from '@chakra-ui/icons'
+import axios from "axios"
+import validateIp from "./utils/validateIp"
 
 function App() {
   const [ipData, setIpData] = useState(null)
   const [search, setSearch] = useState("")
+  const [loading, setLoading] = useState(false);
+
+  const onSearch = async (e) => {
+    e.preventDefault()
+    if (!validateIp(search)) return alert("Invalid IP address")
+
+    try {
+      setLoading(true);
+      const res = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/ip-country-data/${search}`)
+      setIpData({
+        address: search,
+        ...res.data.data
+      })
+    } catch (e) {
+      alert(`Error: ${e.message}`)
+    } finally {
+      setLoading(false)
+    }
+  }
 
   return (
     <Container >
-      <Heading my="8" mb="4" textAlign="center">Rurall IP controll center</Heading>
+      <Heading my="8" mb="4" textAlign="center">Rural IP control center</Heading>
 
       <Box>
-        <Flex justifyContent="center" >
+        <Flex as="form" onSubmit={onSearch} justifyContent="center" >
           <Input value={search} onChange={(e) => setSearch(e.target.value)} type="text" placeholder="181.167.175.214" mr="2" />
-          <IconButton aria-label='Search ip data' icon={<SearchIcon />} mr="2" />
-          <Button>Ban IP</Button>
+          <IconButton isLoading={loading} type="submit" aria-label='Search ip data' icon={<SearchIcon />} mr="2" />
+          <Button type="button">Ban IP</Button>
         </Flex>
       </Box>
-      {ipData &&
+      {ipData !== null &&
         <Box p="4" border="1px" mt="4" borderColor="gray.200" borderRadius="lg">
           <Text fontFamily="monospace" fontSize="md">ip: "{ipData.address}"</Text>
-          <Text fontFamily="monospace" fontSize="md">country: "{ipData.code}"</Text>
+          <Text fontFamily="monospace" fontSize="md">country: "{ipData.name}"</Text>
+          <Text fontFamily="monospace" fontSize="md">country code: "{ipData.code}"</Text>
           <Text fontFamily="monospace" fontSize="md">currency: "{ipData.currency}"</Text>
           <Text fontFamily="monospace" fontSize="md">currency rates:</Text>
-          <Text fontFamily="monospace" fontSize="md"> - USD: "{ipData.currencyRates.USD}"</Text>
-          <Text fontFamily="monospace" fontSize="md"> - EUR: "{ipData.currencyRates.EUR}"</Text>
+          <Text fontFamily="monospace" fontSize="md"> - USD: "{ipData.currencyRates?.USD}"</Text>
+          <Text fontFamily="monospace" fontSize="md"> - EUR: "{ipData.currencyRates?.EUR}"</Text>
         </Box>
       }
     </Container>
