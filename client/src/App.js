c35e23f14866e90aac334cd2796b27c1478f59bd
Matias Zapata <matuzapata@gmail.com>
2022-10-31 19:31:41 -0300

    feat(App.js): Create basic UI layout and setup chakraUI feat(client/** !App.js): create-react-app cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Box, Button, Container, Flex, Heading, IconButton, Input, Text } from "@chakra-ui/react";
+import { SearchIcon } from '@chakra-ui/icons'
 
 function App() {
+  const [ipData, setIpData] = useState(null)
+  const [search, setSearch] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container >
+      <Heading my="8" mb="4" textAlign="center">Rurall IP controll center</Heading>
+
+      <Box>
+        <Flex justifyContent="center" >
+          <Input value={search} onChange={(e) => setSearch(e.target.value)} type="text" placeholder="181.167.175.214" mr="2" />
+          <IconButton aria-label='Search ip data' icon={<SearchIcon />} mr="2" />
+          <Button>Ban IP</Button>
+        </Flex>
+      </Box>
+      {ipData &&
+        <Box p="4" border="1px" mt="4" borderColor="gray.200" borderRadius="lg">
+          <Text fontFamily="monospace" fontSize="md">ip: "{ipData.address}"</Text>
+          <Text fontFamily="monospace" fontSize="md">country: "{ipData.code}"</Text>
+          <Text fontFamily="monospace" fontSize="md">currency: "{ipData.currency}"</Text>
+          <Text fontFamily="monospace" fontSize="md">currency rates:</Text>
+          <Text fontFamily="monospace" fontSize="md"> - USD: "{ipData.currencyRates.USD}"</Text>
+          <Text fontFamily="monospace" fontSize="md"> - EUR: "{ipData.currencyRates.EUR}"</Text>
+        </Box>
+      }
+    </Container>
   );
 }
 
